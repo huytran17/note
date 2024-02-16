5cb6c0470ab0591b55b86f6017a8bd4749e8475d
huytran17 <alpha.lloyd1368@gmail.com>
2024-02-16 21:54:28 +0700

    update app layout

---
+++ dashboard/src/app/layout.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import store from "@/store";
+import { Inter } from "next/font/google";
+import { Provider } from "react-redux";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Provider store={store}>{children}</Provider>
+      </body>
+    </html>
+  );
+}
