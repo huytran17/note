2dfc793edd438087c2b2748d0de551c4109f14bf
huytran17 <alpha.lloyd1368@gmail.com>
2024-02-16 22:01:08 +0700

    note validator

---
+++ server/src/controllers/note/validator/index.ts
@@ -0,0 +1,13 @@
+import createNote from "./create-note";
+import deleteNote from "./delete-note";
+import getNoteById from "./get-note-by-id";
+import updateNote from "./update-note";
+
+export default Object.freeze({
+  createNote,
+  deleteNote,
+  getNoteById,
+  updateNote,
+});
+
+export { createNote, deleteNote, getNoteById, updateNote };
