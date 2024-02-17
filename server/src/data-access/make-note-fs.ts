cee9a97e3018332ef78b74724e7e64b7e55dbaf5
huytran17 <alpha.lloyd1368@gmail.com>
2024-02-17 19:01:17 +0700

    rmv log

--- server/src/data-access/make-note-fs.ts
+++ server/src/data-access/make-note-fs.ts
@@ -87,8 +87,6 @@ export default function makeNoteFs({ filePath }: { filePath: string }) {
           ...finalNoteDetails,
         });
 
-        console.log("---------------------", JSON.stringify(noteContents));
-
         await writeFile(filePath, JSON.stringify(noteContents), {
           encoding: "utf8",
         });
