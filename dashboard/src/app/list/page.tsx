c08a60d261edf752597f2355c7fefc82c85d664c
huytran17 <alpha.lloyd1368@gmail.com>
2024-02-18 15:19:33 +0700

    link list UI

--- dashboard/src/app/list/page.tsx
+++ dashboard/src/app/list/page.tsx
@@ -4,10 +4,15 @@ import { useRootDispatch } from "@/hooks/redux";
 import noteActions from "@/store/note/actions/actions";
 import { useEffect, useState } from "react";
 import Notelist from "@/components/note-list/NoteList";
+import { useRouter } from "next/navigation";
+import styles from "./page.module.css";
 
 export default function ListNotes() {
   const dispatch = useRootDispatch();
   const [notes, setNotes] = useState([]);
+  const router = useRouter();
+
+  const backToPrevPage = () => router.back();
 
   useEffect(() => {
     const fetchNotes = async () => {
@@ -18,5 +23,12 @@ export default function ListNotes() {
     fetchNotes();
   }, []);
 
-  return <div>{<Notelist data={notes} />}</div>;
+  return (
+    <div className={styles["note-list"]}>
+      <div className={styles["back"]}>
+        <button onClick={backToPrevPage}>Back</button>
+      </div>
+      <Notelist data={notes} />
+    </div>
+  );
 }
